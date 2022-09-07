feb2a10292a7246a50db1cea94b1ddc6df90b319
Bice93 <bianca.cerulo@hotmail.it>
2022-09-07 17:11:44 +0200

    Add jumbotron

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,27 @@
 @extends ('layouts.main')
 
 @section('main-content')
-<div class="container">
-    <h2>Lista prodotti</h2>
-    <div class="container_products">
+    <div class="jumbotron"></div>
+    <div class="rowSeries">
+        <div class="container">
+            <h2>Current Series</h2>
+            <div class="container_products">
 
-        @forelse ($elements as $comic)
-        <div class="product">
-            <div class="box_image">
-                <img src="{{ $comic->image}}" alt="Immagine" />
+                @forelse ($elements as $comic)
+                <div class="product">
+                    <div class="box_image">
+                        <img src="{{ $comic->image}}" alt="Immagine" />
+                    </div>
+                    <h6>{{ $comic->title }}</h6>
+                </div>
+                @empty
+                <div>
+                    <h3>Non ci sono elementi disponibili!</h3>
+                </div>
+                @endforelse
             </div>
-            <h6>{{ $comic->title }}</h6>
+            <button>Load More</button>
         </div>
-        @empty
-        <div>
-            <h3>Non ci sono elementi disponibili!</h3>
-        </div>
-        @endforelse
     </div>
-</div>
 
 @endsection
