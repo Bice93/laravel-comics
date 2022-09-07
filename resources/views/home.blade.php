3e1bf4b53788d0047ca387e1dfb09e9cb4042a61
Bice93 <bianca.cerulo@hotmail.it>
2022-09-07 17:00:56 +0200

    Add style home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,23 @@
 @extends ('layouts.main')
 
 @section('main-content')
-    <div>
-        <h2>Lista prodotti</h2>
+<div class="container">
+    <h2>Lista prodotti</h2>
+    <div class="container_products">
+
         @forelse ($elements as $comic)
-        <div>
-            <div>
+        <div class="product">
+            <div class="box_image">
                 <img src="{{ $comic->image}}" alt="Immagine" />
             </div>
             <h6>{{ $comic->title }}</h6>
         </div>
         @empty
-            <div>
-                <h3>Non ci sono elementi disponibili!</h3>
-            </div>
+        <div>
+            <h3>Non ci sono elementi disponibili!</h3>
+        </div>
         @endforelse
     </div>
+</div>
+
 @endsection
